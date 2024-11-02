615885e56eec0961df220f4ef24ea269afc154c2
Furkan <furkan.unsal@tedu.edu.tr>
2024-11-02 08:15:12 +0300

    layout

---
+++ CortexEditorWPF/CortexEditorWPF/View/SceneHierarchy/SceneHierarchy.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CortexEditorWPF.View.SceneHierarchy
+{
+    /// <summary>
+    /// Interaction logic for SceneHierarchy.xaml
+    /// </summary>
+    public partial class SceneHierarchy : UserControl
+    {
+
+        
+        public SceneHierarchy()
+        {
+            InitializeComponent();
+
+            TreeViewItem item = new TreeViewItem();
+            item.Header = "Entity";
+            item.Tag = "Entity";
+
+            SceneHierarchyTree.Items.Add(item);
+        }
+    }
+}
